7eae63e77ad0f9b804a879fda3a57a103b91e429
ho623251 <ho623251@ucf.edu>
2025-01-09 11:32:34 -0500

    Add files via upload

--- Assignment_01/Assignment_01.py
+++ Assignment_01/Assignment_01.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 11:07:46 2025
 
+@author: ashleylau
+
+Jan 09, 2025 
+
+Assignment 1 
+"""
+
+(6 *2 +5)*50 + 1749 +25 - 2003
